b43c2a233e59177df8d99f8dbbbfe82f41348663
Marco Serafini <marcoserafini2@gmail.com>
2021-12-10 10:48:59 +0100

    Add transaction on delete keys

--- it-dgc-verificac19-spring-core/src/main/java/it/dgc/verificac19/data/local/KeyDao.java
+++ it-dgc-verificac19-spring-core/src/main/java/it/dgc/verificac19/data/local/KeyDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface KeyDao extends JpaRepository<Key, String> {
 
+  @Transactional
   void deleteAllByKidNotIn(List<String> keyIds);
 
 }
